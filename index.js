9c9e82a9315c73bf974b944678ebde38db84cdfc
tonybrianmwenda <tonybrianmwenda@gmail.com>
2024-03-24 12:42:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(){
+    return "This Saturday, I want to roller-skate!"
+}
+
+function mondayWork(){
+    return "This Monday, I will go to the office."
+}
+ function wrapAdjective(delimiter){
+    return function(adjective){
+      return "You are " + delimiter + adjective + delimiter + "!"
+    }
+}
+ function saturdayFun(){
+    return "This Saturday, I want to bathe my dog!"
+ }
+
+ 
